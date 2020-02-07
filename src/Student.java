e4c21f4596ada3f2d77219d2691e52ca121d3a89
kpilszak <klaudia.pilszak@gmail.com>
2020-02-07 07:29:12 +0100

    Create fields

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,8 @@
 public class Student {
+    private String firstName;
+    private String lastName;
+    private int studentID;
+    private String courses;
+    private int tuitionBalance;
+    private int costOfCourse = 600;
 }
