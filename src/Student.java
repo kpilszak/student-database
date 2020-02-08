5c20de3f0e9dbdf99db3c51ac005407b952b91d3
kpilszak <klaudia.pilszak@gmail.com>
2020-02-08 09:43:42 +0100

    Initialize Student constructor

--- src/Student.java
+++ src/Student.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
+
 public class Student {
     private String firstName;
     private String lastName;
+    private int gradeYear;
     private int studentID;
     private String courses;
     private int tuitionBalance;
     private int costOfCourse = 600;
+
+    public Student() {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter student first name: ");
+        this.firstName = in.nextLine();
+
+        System.out.print("Enter student last name: ");
+        this.lastName = in.nextLine();
+
+        System.out.print("1 - Freshmen\n2 - Sophmore\n3 - Junior\n4 - Senior\nEnter student class level: ");
+        this.gradeYear = in.nextInt();
+
+        System.out.println(firstName + " " + lastName + " " + gradeYear);
+    }
 }
